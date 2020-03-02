f1f929e10b46642ffb7564d50468746297742fd8
yruel <alexanderstueben@gmx.de>
2020-03-02 16:15:39 +0100

    first commit

---
+++ app/src/main/java/com/github/astueben/inventorymanagement/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.github.astueben.inventorymanagement
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        goToAddProduct.setOnClickListener {
+            startActivity(Intent(this, AddProductActivity::class.java))
+        }
+    }
+}
